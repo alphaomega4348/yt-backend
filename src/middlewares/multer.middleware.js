5e8e9eb51d3875a2f3de57d4e739ba1f6a8082b5
Shivam Kumar <2022ugcs030@nitjsr.ac.in>
2024-05-31 11:07:15 +0530

    minor fixes in file upload

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,14 +1,6 @@
 import multer from "multer";
-import path from "path";
-import {v2 as cloudinary} from 'cloudinary';
-import fs from "fs";
 
-cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api
-    secret: process.env.CLOUDINARY_API_SECRET
-});
+
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
