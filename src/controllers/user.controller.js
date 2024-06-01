c070e2eeebdcd15385ab8524d6098f732f566e44
Shivam Kumar <2022ugcs030@nitjsr.ac.in>
2024-06-01 21:05:45 +0530

    logged out controller added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -154,7 +154,28 @@ const loginUser = asyncHandler(async (req,res)=>{
 })
 const logOutUser = asyncHandler(async (req,res)=>{
     //Acess user to be logout using middlewares
-
+    const userID= req.user._id
+    await User.findByIdAndUpdate(userID,
+      {
+      $set:{
+        refreshToken:undefined
+      }
+    },
+      {
+          new:true 
+    })
+    const options={
+      httpOnly:true,
+      secure:true
+    }
+    return res.
+    status(200).
+    clearCookie("accessToken",options).
+    clearCookie("refreshToken",options).
+    json(
+      new ApiResponse(200,{},"User Logged Out Successfully")
+    )
+    
 
 
 })
---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,29 @@
+import { ApiError } from "../utils/ApiError";
+import { asyncHandler } from "../utils/asyncHandler";
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.model";
+
+export const verifyJWT= asyncHandler(async (req,res,next)=>{
+     try {
+        //cookies not in mobile app
+       const token=req.cookies?.accessToken || req.header("Authorisation")?.replace("Bearer ","")
+       if(!token){
+           throw new ApiError(401,"Unauthorised request");
+       }
+       const decodedToken=  jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
+       if(!decodedToken){
+           throw new ApiError(501,"Error decoding accesstoken")
+       }
+        
+       const user= await User.findById(decodedToken?._id).
+       select(" -password -refreshToken")
+   
+       if(!user){
+           throw new ApiError(401,"Invalid Access Token")
+       }
+       req.user=user
+       next()
+     } catch (error) {
+       throw  new ApiError(401,error?.message|| "Error verifying JWT")
+     }
+})
