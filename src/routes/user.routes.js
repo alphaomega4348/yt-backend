a3cccac8bc570147033c8547fdd403e48076e26c
Shivam Kumar <2022ugcs030@nitjsr.ac.in>
2024-06-02 21:31:03 +0530

    added routes for get channel and history

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -41,9 +41,9 @@ router.route("/refresh-token").post(refreshAccessToken)
 router.route("/change-password").post(verifyJWT, changeCurrentPassword)
 router.route("/current-user").get(verifyJWT, getCurrentUser)
 router.route("/update-account").patch(verifyJWT, updateAccountDetails)
-
 router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
 router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
-
+router.route("/c/:userName").get(verifyJWT,getUserChannelProfile)
+router.route("/c/:history").get(verifyJWT,getWatchHistory)
 
 export default router
